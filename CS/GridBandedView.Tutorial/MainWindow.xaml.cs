ebba7d1bd53cb22c0f2ab7c175208351c3e1b725
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-16 12:42:10 +0300

    initial commit

---
+++ CS/GridBandedView.Tutorial/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GridBandedView.Tutorial {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            DataContext = new DataList();
+            InitializeComponent();
+        }
+    }
+    public class DataList : List<Data> {
+        public DataList() {
+            for(int i = 0; i < 20; i++) {
+                Data d = new Data() {
+                    First = "First #" + i.ToString(),
+                    Second = "Second #" + i.ToString(),
+                };
+                Add(d);
+            }
+        }
+    }
+
+    public class Data {
+        public string First { get; set; }
+        public string Second { get; set; }
+    }
+}
---
+++ VB/GridBandedView.Tutorial/MainWindow.xaml.vb
@@ -0,0 +1,42 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+
+Namespace GridBandedView.Tutorial
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            DataContext = New DataList()
+            InitializeComponent()
+        End Sub
+    End Class
+    Public Class DataList
+        Inherits List(Of Data)
+
+        Public Sub New()
+            For i As Integer = 0 To 19
+                Dim d As New Data() With {.First = "First #" & i.ToString(), .Second = "Second #" & i.ToString()}
+                Add(d)
+            Next i
+        End Sub
+    End Class
+
+    Public Class Data
+        Public Property First() As String
+        Public Property Second() As String
+    End Class
+End Namespace
