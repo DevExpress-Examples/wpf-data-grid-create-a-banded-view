8d4c9c0234cae7025f0750675b4efd886ca8ef85
DevExpressExampleBot <fakeEmail>
2022-12-30 12:08:11 +0400

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,34 +1,35 @@
-﻿Imports System
+Imports System
 Imports System.Collections.Generic
 Imports System.Windows
 Imports System.Windows.Documents
 
 Namespace BandedViewSample
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-			Dim list As New List(Of TestData)()
-			For i As Integer = 0 To 99
-				list.Add(New TestData() With {
-					.Id = i,
-					.Text = "Row" & i,
-					.Number = i,
-					.MultiLineText = "Row" & i & "Line0" & Environment.NewLine & "Row" & i & "Line1" & Environment.NewLine & "Row" & i & "Line2"
-				})
-			Next i
-			DataContext = list
-		End Sub
-	End Class
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-	Public Class TestData
-		Public Property Id() As Integer
-		Public Property Text() As String
-		Public Property Number() As Integer
-		Public Property MultiLineText() As String
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+            Dim list As List(Of TestData) = New List(Of TestData)()
+            For i As Integer = 0 To 100 - 1
+                list.Add(New TestData() With {.Id = i, .Text = "Row" & i, .Number = i, .MultiLineText = "Row" & i & "Line0" & Environment.NewLine & "Row" & i & "Line1" & Environment.NewLine & "Row" & i & "Line2"})
+            Next
+
+            DataContext = list
+        End Sub
+    End Class
+
+    Public Class TestData
+
+        Public Property Id As Integer
+
+        Public Property Text As String
+
+        Public Property Number As Integer
+
+        Public Property MultiLineText As String
+    End Class
 End Namespace
